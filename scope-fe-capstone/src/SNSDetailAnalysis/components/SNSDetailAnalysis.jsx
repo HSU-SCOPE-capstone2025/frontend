c090475d8f407683c6a8c39c7ac6ea20b1dd77b2
yeeun <yeeun@yeeunui-MacBookAir.local>
2025-03-29 12:30:52 +0900

    sns 상세 페이지 디자인 수정

--- scope-fe-capstone/src/SNSDetailAnalysis/components/SNSDetailAnalysis.jsx
+++ scope-fe-capstone/src/SNSDetailAnalysis/components/SNSDetailAnalysis.jsx
@@ -4,6 +4,7 @@ import { FaInstagram, FaYoutube, FaTiktok } from "react-icons/fa";
 import "../css/snsdetailanalysis.css"; // CSS 파일 추가
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
 
+
 //img파일 import 
 import img1 from "../../assets/images/main2.png";
 import img2 from "../../assets/images/main2.png";
@@ -12,6 +13,12 @@ import img4 from "../../assets/images/main2.png";
 import img5 from "../../assets/images/main2.png";
 import img6 from "../../assets/images/main2.png";
 import influencer from "../../assets/images/influencer.png";
+import heart from "../../assets/images/heart.png";
+import person from "../../assets/images/person.png";
+import good from "../../assets/images/good.png";
+import comment from "../../assets/images/comment.png";
+import dataComment from "../../assets/images/data_comment.png";
+
 // SNS 로고 이미지 import
 import instagramLogo from "../../assets/images/instagram_logo.png";
 import youtubeLogo from "../../assets/images/youtube_logo.png"; 
@@ -33,6 +40,9 @@ const SNSDetailAnalysis = () => {
   const analysisTags=["#다정한  #귀여운  #부드러운"]
   const [activeSNS, setActiveSNS] = useState(null);
   const instaPost=[1042];
+  const [currentPage, setCurrentPage] = useState(1); // 현재 페이지
+  const imagesPerPage = 5; // 한 페이지당 보여줄 이미지 개수
+
   // 더미 데이터
   const data = {
     followers: [15000, 30000, 45000, 70000, 110000, 100600, 210000,300000,300000,400000,200100,300000,300000,400000,400500,400005,300000,400000,450000,400005,300000],
@@ -60,6 +70,10 @@ const SNSDetailAnalysis = () => {
     ],
   };
 
+  const indexOfLastImage = currentPage * imagesPerPage;
+  const indexOfFirstImage = indexOfLastImage - imagesPerPage;
+  const currentImages = images.slice(indexOfFirstImage, indexOfLastImage);
+  const totalPages = Math.ceil(images.length / imagesPerPage);
 
   const options = {
     responsive: true,
@@ -86,6 +100,7 @@ const SNSDetailAnalysis = () => {
       x: {
         grid: {
           display: false,
+
         },
         ticks: {
           color: "#000",
@@ -98,7 +113,11 @@ const SNSDetailAnalysis = () => {
       },
       y: {
         grid: {
-          color: "#CECECE",
+          drawBorder: false,
+          drawOnChartArea: true,     // ✅ 차트 영역 안에도 선을 그림
+          color: '#CECECE',          // ✅ 선 색상
+          borderDash: [4, 4],        // ✅ 점선: [선 길이, 간격]
+
         },
         ticks: {
           color: "#000",
@@ -247,28 +266,58 @@ const SNSDetailAnalysis = () => {
             <div className="section-title">분석 리포트</div>
             <div className="section-sub-title">최근 활동 추이</div>
             <div className="tab-buttons">
-              <button className={activeTab === "followers" ? "active" : ""} onClick={() => setActiveTab("followers")}>
-                <div className="total-people-number">팔로워</div>
+  <button
+    className={`tab-button ${activeTab === "followers" ? "active" : ""}`}
+    onClick={() => setActiveTab("followers")}
+  >
+   <div className="tab-content">
+    <div className="tab-title">팔로워</div>
+    <div className="tab-value">
+      <span className="tab-number">32.4</span>
+      <span className="tab-unit">만</span>
+      </div>
 
-                <div className="detail-tab-button-container">
-                  <span className="detail-numbering">32.4</span><span className="detail-unit">만</span>
-
-                  </div>
+    </div>
+    <img src={person} alt="person" className="person-icon" />
 
 
-              </button>
-              <button className={activeTab === "likes" ? "active" : ""} onClick={() => setActiveTab("likes")}>
-              <div className="average-likes-number">평균 좋아요</div>
-              <span className="detail-numbering">32.4</span><span className="detail-unit">만</span>
 
 
-              </button>
-              <button className={activeTab === "comments" ? "active" : ""} onClick={() => setActiveTab("comments")}>
-              <div className="average-comment-number">평균 댓글</div>
-              <span className="detail-numbering">32.4</span><span className="detail-unit">만</span>
+  </button>
 
-              </button>
-            </div>
+  <button
+    className={`tab-button ${activeTab === "likes" ? "active" : ""}`}
+    onClick={() => setActiveTab("likes")}
+  >
+       <div className="tab-content">
+
+    <div className="tab-title">평균 좋아요</div>
+    <div className="tab-value">
+      <span className="tab-number">32.4</span>
+      <span className="tab-unit">만</span>
+      </div>
+    </div>
+    <img src={good} alt="good" className="person-icon" />
+
+  </button>
+
+  <button
+    className={`tab-button ${activeTab === "comments" ? "active" : ""}`}
+    onClick={() => setActiveTab("comments")}
+  >
+       <div className="tab-content">
+
+    <div className="tab-title">평균 댓글</div>
+    <div className="tab-value">
+      <span className="tab-number">32.4</span>
+      <span className="tab-unit">만</span>
+    </div>
+    </div>
+    <img src={comment} alt="" className="person-icon" />
+
+  </button>
+</div>
+
             <Line data={graphData} options={options} className="chart" />
             </div>
 
@@ -339,7 +388,7 @@ const SNSDetailAnalysis = () => {
 
 
           <div className="image-grid-title">릴스 이미지</div>
-          <div className="image-grid">
+          {/* <div className="image-grid">
       {images.map((image, index) => (
         <div 
           key={index} 
@@ -347,17 +396,74 @@ const SNSDetailAnalysis = () => {
           style={{ backgroundImage: `url(${image})` }} 
         />
       ))}
-         </div>
-         <button className="more-shorts-image"> 더보기</button>
+         </div> */}
+
+        <div className="image-grid">
+          {currentImages.map((image, index) => ( // 기존 images.map → currentImages.map
+            <div 
+              key={index} 
+              className="sns-content-img" 
+              style={{ backgroundImage: `url(${image})` }} 
+            />
+          ))}
+        </div>
+          <div className="pagination">
+            <button onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}>&lt;</button>
+            {[...Array(totalPages)].map((_, index) => (
+              <button
+                key={index}
+                className={currentPage === index + 1 ? "active" : ""}
+                onClick={() => setCurrentPage(index + 1)}
+              >
+                {index + 1}
+              </button>
+            ))}
+            <button onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}>&gt;</button>
           </div>
+          </div>
+
+       
+
           <div className="statistics">
           <div className="stat-box">
             <p className="stat-label">총합 데이터</p>
-            <p className="stat-value">좋아요 ❤️ 18,765 개</p>
+            <div className="stat-containers">
+            <div className="stat-value-container">
+            <img src={heart} alt="heart" className="data-heart-icon" />
+
+            <p className="stat-value">좋아요</p>
+            <p className="stat-good-value">12345 개</p>
+            </div>
+            <div className="stat-value-container">
+
+            <img src={dataComment} alt="dataComment" className="data-comment-icon" />
+
+              <p className="stat-value">댓글</p>
+              <p className="stat-comment-value">12345 개
+              </p>
+            </div>
+            </div>
+
           </div>
           <div className="stat-box">
-            <p className="stat-label">평균 댓글</p>
-            <p className="stat-value">댓글 ❤️ 2,890 개</p>
+          <p className="stat-label">평균 데이터</p>
+          <div className="stat-containers">
+            <div className="stat-value-container">
+            <img src={heart} alt="heart" className="data-heart-icon" />
+
+            <p className="stat-value">좋아요</p>
+            <p className="stat-good-value">12345 개</p>
+            </div>
+            <div className="stat-value-container">
+
+            <img src={dataComment} alt="dataComment" className="data-comment-icon" />
+
+              <p className="stat-value">댓글</p>
+              <p className="stat-comment-value">12345 개
+              </p>
+            </div>
+            </div>
+
           </div>
           </div>
           <div className="stat-box-tag">
