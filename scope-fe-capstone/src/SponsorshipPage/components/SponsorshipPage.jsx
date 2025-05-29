d9cf4140afdd2bd3838c417e0912e65976d5b5bd
yeeun <yeeun@yeeunui-MacBookAir.local>
2025-05-29 16:33:10 +0900

    협찬이력 페이지 인플루언서 이름 변경

--- scope-fe-capstone/src/SponsorshipPage/components/SponsorshipPage.jsx
+++ scope-fe-capstone/src/SponsorshipPage/components/SponsorshipPage.jsx
@@ -9,10 +9,43 @@ import dummyData from "../../data/dummy.json"; // 데이터는 별도 파일로
 const SponsorshipPage = () => {
   const { id } = useParams();
   const data = dummyData[id] || [];
+  const nameMap = {
+    "__ralral__": "랄랄",
+    "horseking123": "말왕",
+    "joodoong_e": "주둥이방송",
+    "chimchakman_official": "침착맨",
+    "trytoeat222": "먹어볼래 TryToEat",
+    "under_world_b1": "언더월드",
+    "hee__bab": "히밥",
+    "cherry__0705": "지한",
+    "yellow_aquarium": "해수인",
+    "esther__park": "박에스더",
+    "very_simplecooking": "자취요리신",
+    "minsco_": "민스코",
+    "instawakgood": "우왁굳",
+    "yooxicman": "육식맨",
+    "dudely_08": "더들리",
+    "fundamental_kr": "뻔더",
+    "jesusujeo": "은수저",
+    "tikitakabooboo": "인생 녹음 중",
+    "yonamism": "취미로 요리하는 남자",
+    "mejooandcats": "김메주와 고양이들",
+    "jeon_unni_": "젼언니",
+    "bboyongnyong": "뽀용뇽",
+    "salim_nam_official": "살림남",
+    "minsajang_": "요리하는 민사장",
+    "enjoycouple": "엔조이커플",
+    "hxxax_": "혜안",
+    "g_movie__": "지무비",
+    "hugbears": "달달투어",
+    "ipduck_official": "입시덕후",
+    "baby_pig.rabbit": "돼끼"
+  };
 
+  const displayName = nameMap[id] || id;
   return (
     <div className="sponsorship-page-container">
-      <h2 className="sponsorship-title">{id}님의 협찬 이력</h2>
+      <h2 className="sponsorship-title">{displayName}님의 협찬 이력</h2>
       {data.length > 0 ? (
         <div className="sponsorship-card-container">
           {data.map((item, index) => (
