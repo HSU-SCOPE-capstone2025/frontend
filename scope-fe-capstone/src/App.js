9900738d04e9d081f23f1db202f7556ce682c433
mimolulu <162582910+mimolulu@users.noreply.github.com>
2025-07-11 20:40:14 +0900

    Update App.js
    로그인 수정 유빈

--- scope-fe-capstone/src/App.js
+++ scope-fe-capstone/src/App.js
@@ -1,4 +1,58 @@
-// src/App.js
+// // src/App.js
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { UserProvider } from './contexts/UserContext';
+// import "./App.css";
+// import Nav from './component/Nav';
+// import ScopeMain from './ScopeMain/components/ScopeMain';
+// import InfluencerRanking from './InfluencerRanking/components/InfluencerRanking';
+// import ProfileDetailAnalysis from './ProfileDetailAnalysis/components/ProfileDetailAnalysis';
+// import SNSDetailAnalysis from './SNSDetailAnalysis/components/SNSDetailAnalysis';
+// import Analysis from './Analysis/components/Analysis';
+// import Recommendation from './Recommendation/components/Recommendation';
+// import Register from './Register/components/Register';
+// import Login from './Login/component/Login';
+// import SponsorshipPage from './SponsorshipPage/components/SponsorshipPage';
+// //임시 추가
+// import DetailAnalysis from './DetailAnalysis/components/DetailAnalysis';
+// import SNSContent from './DetailAnalysis/components/SNSContent';
+// //import AccountContent from './DetailAnalysis/components/AccountContent';
+// import ScopeChatbotPage from './ScopeChatbotPage/components/ScopeChatbotPage';
+
+
+// // 전체 App 컴포넌트
+// const App = () => {
+//   return (
+//     <UserProvider>
+//        <Router>
+//       <Nav />
+//       <Routes>
+//         {/* Public Routes */}
+//         <Route path="/" element={<ScopeMain />} />
+
+//         {/* Protected Routes */}
+//         <Route path="/Analysis" element={<Analysis />} />
+//         <Route path="/InfluencerRanking" element={<InfluencerRanking />} />
+//         <Route path="/ProfileDetailAnalysis" element={<ProfileDetailAnalysis />} />
+//         <Route path="/SNSDetailAnalysis" element={<SNSDetailAnalysis />} />
+//         <Route path="/Recommendation" element={<Recommendation />} />
+//         <Route path="/ScopeChatbotPage" element={<ScopeChatbotPage />} />
+//         {/* <Route path="/DetailAnalysis" element={<DetailAnalysis />} /> */}
+//         <Route path="/DetailAnalysis/:id" element={<DetailAnalysis />} />
+//         <Route path="/Register" element={<Register />} />  
+//         <Route path="/Login" element={<Login />} />  
+//         <Route path="/sponsorships/:id" element={<SponsorshipPage />} />
+//         <Route path="/influencer/:id" element={<SNSContent />} />
+
+//       </Routes>
+//     </Router>
+//     </UserProvider>
+   
+//   );
+// };
+
+// export default App;
+
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { UserProvider } from './contexts/UserContext';
@@ -13,41 +67,37 @@ import Recommendation from './Recommendation/components/Recommendation';
 import Register from './Register/components/Register';
 import Login from './Login/component/Login';
 import SponsorshipPage from './SponsorshipPage/components/SponsorshipPage';
-//임시 추가
 import DetailAnalysis from './DetailAnalysis/components/DetailAnalysis';
 import SNSContent from './DetailAnalysis/components/SNSContent';
-//import AccountContent from './DetailAnalysis/components/AccountContent';
 import ScopeChatbotPage from './ScopeChatbotPage/components/ScopeChatbotPage';
 
-
-// 전체 App 컴포넌트
 const App = () => {
   return (
     <UserProvider>
-       <Router>
-      <Nav />
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<ScopeMain />} />
+      <Router>
+        <Nav />
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<ScopeMain />} />
 
-        {/* Protected Routes */}
-        <Route path="/Analysis" element={<Analysis />} />
-        <Route path="/InfluencerRanking" element={<InfluencerRanking />} />
-        <Route path="/ProfileDetailAnalysis" element={<ProfileDetailAnalysis />} />
-        <Route path="/SNSDetailAnalysis" element={<SNSDetailAnalysis />} />
-        <Route path="/Recommendation" element={<Recommendation />} />
-        <Route path="/ScopeChatbotPage" element={<ScopeChatbotPage />} />
-        {/* <Route path="/DetailAnalysis" element={<DetailAnalysis />} /> */}
-        <Route path="/DetailAnalysis/:id" element={<DetailAnalysis />} />
-        <Route path="/Register" element={<Register />} />  
-        <Route path="/Login" element={<Login />} />  
-        <Route path="/sponsorships/:id" element={<SponsorshipPage />} />
-        <Route path="/influencer/:id" element={<SNSContent />} />
+          {/* 기능 페이지 라우팅 */}
+          <Route path="/Analysis" element={<Analysis />} />
+          <Route path="/InfluencerRanking" element={<InfluencerRanking />} />
+          <Route path="/ProfileDetailAnalysis" element={<ProfileDetailAnalysis />} />
+          <Route path="/SNSDetailAnalysis" element={<SNSDetailAnalysis />} />
+          <Route path="/Recommendation" element={<Recommendation />} />
+          <Route path="/ScopeChatbotPage" element={<ScopeChatbotPage />} />
+          <Route path="/DetailAnalysis/:id" element={<DetailAnalysis />} />
+          <Route path="/Register" element={<Register />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/sponsorships/:id" element={<SponsorshipPage />} />
+          <Route path="/influencer/:id" element={<SNSContent />} />
 
-      </Routes>
-    </Router>
+          {/*로그인 후 리다이렉션용 동적 URL */}
+          <Route path="/:influencerId" element={<ScopeMain />} />
+        </Routes>
+      </Router>
     </UserProvider>
-   
   );
 };
 
